7b877d3c13f3f7434f7e6d0c3dec34ef689a91dd
Daniel Shin <kyuseung1016@gmail.com>
2025-01-21 23:51:06 -1000

    tests for formatting

--- src/format.rs
+++ src/format.rs
@@ -28,3 +28,44 @@ impl Formatter {
 
     // pub fn to_bedrock_input
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use chrono::{TimeZone, Utc};
+
+    fn get_sample_arxiv() -> ArxivResult {
+        ArxivResult {
+            title: "title".to_string(),
+            summary: "summary".to_string(),
+            authors: vec!["john doe".to_string()],
+            published: Utc.timestamp_opt(0, 0).unwrap(),
+            link: "www.example.com".to_string()
+        }
+    }
+
+    const BASE_README: &str = concat!(
+        "### title\n_john doe_<br/>\nsummary<br/>\n_Published: 1970.01.01_, ",
+        "[www.example.com](www.example.com)\n\n"
+    );
+
+    const BASE_JSONL: &str = concat!(
+        "{\"id\": 0, \"title\": \"title\", \"authors\": [\"john doe\"], \"summary\": \"summary\", ",
+        "\"pub\": \"1970.01.01\", \"link\": \"www.example.com\"}\n"
+    );
+
+    #[test]
+    fn test_readme() {
+        let base = String::from(BASE_README);
+        let readme = Formatter::to_readme(&get_sample_arxiv());
+        assert_eq!(base, readme);
+    }
+
+    #[test]
+    fn test_jsonl() {
+        let base = String::from(BASE_JSONL);
+        let jsonl = Formatter::to_jsonl_with_id(0, &get_sample_arxiv());
+        assert_eq!(base, jsonl);
+    }
+}
